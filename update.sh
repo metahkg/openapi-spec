33196d80f1ae26483c4449ce56b0f67ad0768c91
wcyat <wcyat@wcyat.me>
2022-11-06 16:11:27 +0800

    auto update and trigger metahkg-docs

---
+++ update.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+wget "https://gitlab.com/metahkg/metahkg-server/-/raw/${branch}/openapi.yaml"
+version=$(cat openapi.yaml | grep version | awk -F: '{ print $2 }' | sed 's/[" ]//g' | sed 's/-dev//g')
+suffix=$(if [ "$branch" = "dev" ]; then echo "-dev"; else echo ""; fi;)
+minor="v$(node -e "console.log('${version}'.split('.').slice(0, 2).join('.'))")${suffix}"
+
+if [[ -d "${minor}" ]];
+then
+  mv openapi.yaml "${minor}";
+else
+  mkdir "$minor" && mv openapi.yaml "${minor}";
+fi;
+
+export minor="$minor"
+
+rm -f openapi.yaml;
